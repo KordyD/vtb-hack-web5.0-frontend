e7ebd64231825c9eae14e96c1eed4c4da0ad8f8b
svankovich <svankovich@devexperts.com>
2023-10-14 01:09:11 +0300

    Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,47 @@
+import { MainMenu, MenuItem } from "./menu/MainMenu";
+
+
+
 function App() {
-  return <></>;
+  const menuItems: MenuItem[] = [
+    {name: "Кредиты", subItems: [
+      {name: "Кредит наличными"}, 
+      {name: "Экспресс кредит"},
+      {name: "Рефинансирование"},
+    ]},
+    {name: "Вклады", subItems: [ {name: "Рассчитать кредит"}, {name: "Кредит наличными"} ]},
+    {name: "Карты", subItems: [
+      {name: "Дебетовые карты"},
+      {name: "Кредитные карты"},
+      {name: "Пенсионные карты"},
+      {name: "Карты жителя"},
+    ]},
+    {name: "Ипотека", subItems: []},
+    {name: "Автокредиты", subItems: [
+      {name: "Автокредит наличными"}, 
+      {name: "Автокредит в автосалоне"}, 
+    ]},
+    {name: "Вклады и счета", subItems: []},
+    {name: "Инвестиции", subItems: []},
+    {name: "Платежи и переводы", subItems: [
+      {name: "Оплата услуг"}, 
+    ]},
+    {name: "Другие услуги", subItems: [
+      {name: "Страховые и сервисные продукты"},
+      {name: "Аренда сейфовых ячеек"},
+      {name: "Банкротство физических лиц"}, 
+    ]},
+    {name: "Валюта и золото", subItems: [
+      {name: "Купить валюту"},
+      {name: "Продать валюту"},
+      {name: "Купить золото"}, 
+    ]},
+  ]
+  return (
+      <div>
+        <MainMenu menuItems={menuItems} />
+      </div>
+    );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import "./fonts/VTBGroupUI-Regular/VTBGroupUI-Regular.ttf"
+import "./fonts/VTBGroupUI-Medium/VTBGroupUI-Medium.ttf"
+import "./fonts/VTBGroupUI-Light/VTBGroupUI-Light.ttf"
+import "./fonts/VTBGroupUI-Bold/VTBGroupUI-Bold.ttf"
+import "./fonts/VTBGroupUI-DemiBold/VTBGroupUI-DemiBold.ttf"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
